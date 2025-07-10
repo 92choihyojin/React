8f8c7f8e5cfa3b14522b6e598f0e477daea90c9a
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-07-10 15:23:17 +0900

    리액트 - URL 파라미터 Params , button을 이용한 페이지 이동2

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,13 +1,18 @@
 import { useSearchParams } from "react-router-dom";
 
 const New = () => {
+	//useState() 비슷한 기능을 url 처리하는데 사용되어진다.
 	const [searchParams, setSearchParams]= useSearchParams();
-
-  return(
-		<div>
-			New
-		</div>
-	);
+	const updateNameParam = (name, age) =>{
+		setSearchParams({name: name, age: age});
+	};
+  return (
+    <div>
+      New?{searchParams.get("name")} <br />
+      New?{searchParams.get("age")} <br />
+      <button onClick={()=>{updateNameParam("chj","34")}}>쿼리스트링 값 변경하기</button>
+    </div>
+  );
 };
 
 export default New;
