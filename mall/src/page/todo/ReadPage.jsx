3e000d82797a20e0982307becb111fa9fd2a0fd6
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-07-15 15:15:08 +0900

    mall - 버튼을 누르면 리스트로 가는거, 수정으로 가는거

--- mall/src/page/todo/ReadPage.jsx
+++ mall/src/page/todo/ReadPage.jsx
@@ -1,16 +1,61 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "../../include/Header";
-import { useParams } from "react-router-dom";
+import {
+  createSearchParams,
+  useParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import { useCallback, useMemo } from "react";
 
-const ReadPage= ()=> {
+const ReadPage = () => {
   const { tno } = useParams();
+  const navigate = useNavigate();
+  const [queryParam] = useSearchParams();
+  const page = queryParam.get("page") ? parseInt(queryParam.get("page")) : 1;
+  const size = queryParam.get("size") ? parseInt(queryParam.get("size")) : 10;
+  const queryStr = useMemo(() => {
+    return createSearchParams({ page, size }).toString();
+  }, [page, size]);
+
+  const moveToModify = useCallback(
+    (tno) => {
+      navigate({ pathname: `/todo/modify/${tno}`, search: queryStr });
+    },
+    [navigate, queryStr]
+  );
+
+  const moveToList = useCallback(() => {
+    navigate({ pathname: `/todo/list`, search: queryStr });
+  }, [page, size]);
+
   return (
     <Container>
       <Header />
       To do ReadPage {tno}
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button
+          class="btn btn-outline-success"
+          type="button"
+          onClick={() => {
+            moveToModify(tno);
+          }}
+        >
+          Test Button Modify
+        </button>
+        <button
+          class="btn btn-outline-danger"
+          type="button"
+          onClick={() => {
+            moveToList();
+          }}
+        >
+          Test Button List
+        </button>
+      </div>
     </Container>
   );
-}
+};
 
 export default ReadPage;
